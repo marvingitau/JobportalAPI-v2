113aca7c725bbe4b4781ef908bcae7042b0c58f1
marvingitau <marvingitau96@gmail.com>
2022-04-21 18:25:58 +0300

    requision controller

---
+++ Controllers/StaffRequisionController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RPFBE.Controllers
+{
+    public class StaffRequisionController : Controller
+    {
+        private readonly ICodeUnitWebService codeUnitWebService;
+
+        public StaffRequisionController( 
+            ICodeUnitWebService codeUnitWebService
+            )
+        {
+            this.codeUnitWebService = codeUnitWebService;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

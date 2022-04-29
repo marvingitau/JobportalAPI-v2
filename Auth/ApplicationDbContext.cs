be4743bedc0f850f55c22bd0a7ef456b52d9f8a5
marvingitau <marvingitau96@gmail.com>
2022-04-29 16:59:10 +0300

    Added requistion progress

--- Auth/ApplicationDbContext.cs
+++ Auth/ApplicationDbContext.cs
@@ -19,6 +19,8 @@ namespace RPFBE.Auth
         public DbSet<Skill> Skills { get; set; }
         public DbSet<JobSpecFile> SpecFiles { get; set; }  
         public DbSet<UserCV> UserCVs { get; set; }
+        public DbSet<RequisitionProgress> RequisitionProgress { get; set; }
+
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);

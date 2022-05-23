c92c28a2a3132e6a853f3a07955da0620d779776
marvingitau <marvingitau96@gmail.com>
2022-05-23 07:46:50 +0300

    apprval workflow

--- Migrations/20220518050657_approval workflow stripping.Designer.cs
+++ Migrations/20220518050657_approval workflow stripping.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RPFBE.Auth;
 
 namespace RPFBE.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220518050657_approval workflow stripping")]
+    partial class approvalworkflowstripping
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

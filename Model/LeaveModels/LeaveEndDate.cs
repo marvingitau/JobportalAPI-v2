c2708894f216bb9a5c3d724abe27fd7c232c44ee
marvingitau <marvingitau96@gmail.com>
2022-06-22 17:24:22 +0300

    leave and doc mgt

---
+++ Model/LeaveModels/LeaveEndDate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RPFBE.Model.LeaveModels
+{
+    public class LeaveEndDate
+    {
+        public int Id { get; set; }
+        public string EmployeeNo { get; set; }
+        public string LeaveType { get; set; }
+        public DateTime LeaveStartDate { get; set; }
+        public int DaysApplied { get; set; }
+        public string LeaveAppNo { get; set; }
+        public string RelieverNo { get; set; }
+        public string RelieverRemark { get; set; }
+    }
+}

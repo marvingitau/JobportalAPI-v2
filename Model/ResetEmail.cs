49f80528d02dda55de33a3e87c7419b1ad65a886
marvingitau <marvingitau96@gmail.com>
2022-07-06 08:14:30 +0300

    Performance Rlated

--- Model/ResetEmail.cs
+++ Model/ResetEmail.cs
@@ -8,6 +8,8 @@ namespace RPFBE.Model
     public class ResetEmail
     {
         public string EmployeeId { get; set; }
+        public string Token { get; set; }
+        public string Password { get; set; }
 
         //Honey pot
         public bool Approved { get; set; }
